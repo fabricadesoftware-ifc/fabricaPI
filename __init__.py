755effbb32f67e5942e7d17b6ba93bf15a3e9324
mateus-lopes <mateusalbano22@gmail.com>
2024-03-12 17:13:21 -0300

    FEAT: Adding streamlit boilerplate

---
+++ __init__.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+from streamlit.logger import get_logger
+
+LOGGER = get_logger(__name__)
+
+
+def run():
+    st.set_page_config(
+        page_title="Hello",
+        page_icon="👋",
+    )
+
+    st.write("# Welcome to Streamlit! 👋")
+
+    st.sidebar.success("boilerplate is running!")
+
+
+if __name__ == "__main__":
+    run()
