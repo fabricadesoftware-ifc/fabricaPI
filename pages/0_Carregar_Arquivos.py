88304d5f51f9d12c41c89ac8fc28b52eae696e9c
mateus-lopes <mateusalbano22@gmail.com>
2024-03-13 20:20:07 -0300

    FEAT: Multipages and clean code

---
+++ pages/0_Carregar_Arquivos.py
@@ -0,0 +1,68 @@
+import streamlit as st
+from streamlit.logger import get_logger
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import numpy as np
+
+def run():
+    st.set_page_config(
+        page_title="Campus PI App | Carregar Arquivos", 
+        page_icon="📃", 
+        layout="wide", 
+        initial_sidebar_state="expanded", 
+        menu_items={
+            'Get Help': 'https://www.extremelycoolapp.com/help',
+            'Report a bug': "https://www.extremelycoolapp.com/bug",
+            'About': """
+                Este é um Projeto de Pesquisa desenvolvido para visualização, análise e transparência de dados do ifc araquari.
+                \\
+                \\
+                Professor Responsavel: [Fábio Longo de Moura](www.github.com/ldmfabio) 
+                \\
+                Aluno Responsavel: [Mateus Lopes Albano](www.github.com/mateus-lopes)
+                \
+                \
+
+            """
+        }
+    )
+    with st.sidebar:
+        pass
+
+    st.write("## 📃 Carregar Arquivos")
+
+    if not st.session_state.get('uploaded_files'):
+        st.session_state.uploaded_files = []
+    
+    error_file = False
+    
+    files = st.file_uploader("Escolha os Arquivos CSV", accept_multiple_files=True, help="Arraste e solte os arquivos aqui ou clique para fazer upload.")
+
+    col1, col2, col3, col4, col5, col6 = st.columns(6)
+
+    with col1:
+        if st.button('Carregar arquivos', type="secondary"):
+            if files:
+                st.session_state.uploaded_files = files
+            else:
+                error_file = True
+
+    with col2:
+        if st.session_state.uploaded_files:
+            if st.button('Limpar Escolha', type="secondary"):
+                st.session_state.uploaded_files = []
+
+    if error_file:
+        st.write("( *Nenhum arquivo foi enviado* )")
+
+    if st.session_state.uploaded_files:
+        st.write("##### **Arquivos Carregados:**")
+        st.write(f"( *{len(st.session_state.uploaded_files)} Arquivos Carregados* )")
+        for obj in st.session_state.uploaded_files:
+            st.write(f"- {obj.name}")
+
+        st.session_state.data_frames = [pd.read_csv(i, encoding='latin-1', sep=';') for i in st.session_state.uploaded_files]
+
+if __name__ == "__main__":
+    run()
---
+++ pages/3_Resultado.py
@@ -0,0 +1,69 @@
+from urllib.error import URLError
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import streamlit as st
+from streamlit.hello.utils import show_code
+
+
+def data_frame_demo():
+    @st.cache_data
+    def get_UN_data():
+        df = pd.concat(st.session_state.data_frames, ignore_index=True)
+        st.write(df)
+        return df.set_index("NO_STATUS_MATRICULA")
+
+    try:
+        df = get_UN_data()
+        status = st.multiselect(
+            "Filtrar por status da matricula", ["ABANDONO", "TRANSF_EXT", "CONCLUÍDA", "DESLIGADO", "EM_CURSO"], ["EM_CURSO"]
+        )
+        if not status:
+            st.error("Please select at least one NO_STATUS_MATRICULA.")
+        else:
+            data = df.loc[status]
+            st.write("### Número de alunos por status da matricula", data.sort_index())
+
+            df_alunos_abandono = df[df.index == 'ABANDONO'].shape[0]
+            df_alunos_transf_ext = df[df.index == 'TRANSF_EXT'].shape[0]
+            df_alunos_concluida = df[df.index == 'CONCLUÍDA'].shape[0]
+            df_alunos_desligado = df[df.index == 'DESLIGADO'].shape[0]
+            df_alunos_em_curso = df[df.index == 'EM_CURSO'].shape[0]
+
+            df_NO_STATUS_MATRICULA = pd.DataFrame({
+                'NO_STATUS_MATRICULA': ['ABANDONO', 'TRANSF_EXT', 'CONCLUÍDA', 'DESLIGADO', 'EM_CURSO'],
+                'Total': [df_alunos_abandono, df_alunos_transf_ext, df_alunos_concluida, df_alunos_desligado, df_alunos_em_curso]
+            })
+
+            fig, ax = plt.subplots(figsize=(8, 4))
+            ax.barh(df_NO_STATUS_MATRICULA["NO_STATUS_MATRICULA"], df_NO_STATUS_MATRICULA['Total'])
+            ax.set_title('')
+            ax.set_xlabel('Quantidade de alunos')
+            ax.set_ylabel('')
+
+            for i,v in enumerate(df_NO_STATUS_MATRICULA['Total']):
+                ax.text(v + 1, i, str(v), color="black", fontsize=8, ha="left", va="center")
+
+
+            ax.yaxis.set_tick_params(labelsize=12)
+            ax.xaxis.set_tick_params(labelsize=12)
+            ax.spines['top'].set_visible(False)
+            ax.spines['right'].set_visible(False)
+
+            st.pyplot(fig)
+    except URLError as e:
+        st.error(
+            """
+            **This demo requires internet access.**
+            Connection error: %s
+        """
+            % e.reason
+        )
+
+
+st.set_page_config(page_title="DataFrame Demo", page_icon="✅")
+st.markdown("## ✅ Resultado dos DataFrames")
+
+
+if __name__ == "__main__":
+    data_frame_demo()
