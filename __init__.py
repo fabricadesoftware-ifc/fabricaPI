c14dea91c972c0768d94588daf38cff5aa65fdc7
mateus-lopes <mateusalbano22@gmail.com>
2024-03-13 09:26:07 -0300

    CHORE: Adding filters and concatenating the files

--- __init__.py
+++ __init__.py
@@ -1,39 +1,16 @@
 import streamlit as st
-
 from streamlit.logger import get_logger
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import numpy as np
+
+import options.filters as filters
+import options.configs as configs
 
 LOGGER = get_logger(__name__)
 
-def reset_sidebar_options():
-    st.session_state.is_running = False
-
-def run():
-    st.set_page_config(
-        page_title="Campus PI App",
-        page_icon="📈",
-        layout="wide",
-        initial_sidebar_state="expanded",
-        menu_items={
-            'Get Help': 'https://www.extremelycoolapp.com/help',
-            'Report a bug': "https://www.extremelycoolapp.com/bug",
-            'About': """
-                Este é um Projeto de Pesquisa desenvolvido para visualização, análise e transparência de dados do ifc araquari.
-                \\
-                \\
-                Professor Responsavel: [Fábio Longo de Moura](www.github.com/ldmfabio) 
-                \\
-                Aluno Responsavel: [Mateus Lopes Albano](www.github.com/mateus-lopes)
-                \
-                \
-
-            """
-        }
-    )
-
-    if "is_running" not in st.session_state:
-        st.session_state.is_running = False
-
-    def runPage():
+def runPage():
         st.markdown(
             """
             ## 📈 Projeto Campus PI  
@@ -57,53 +34,81 @@ def run():
         with col2:
             st.image('./assets/img/logo-fabrica.png')
 
-    def runGraph():
-        st.write('RODANDO GRAFICO...')
-        st.write(st.session_state.selected_filter)
-        st.write(st.session_state.uploaded_files)
+def apply_filters(df, translate_label):
+    filters = {
+        'status': f"NO_STATUS_MATRICULA == '{translate_label.get(st.session_state.selected_filter_status, '')}'", 
+        'year': f"CO_CICLO_MATRICULA != '{translate_label.get(st.session_state.selected_filter_year, '')}'", 
+        'curse': f"NO_STATUS_MATRICULA != '{translate_label.get(st.session_state.selected_filter_curse, '')}'"
+    }
+
+    query = ' and '.join(f'({condition})' for condition in filters.values())
+
+    if query:
+        df = df.query(query)
+
+    return df
+
+def runGraph(translate_label):
+    if not st.session_state.get('uploaded_files'):
+        st.write("Nenhum arquivo foi enviado")
+        return
+
+    files = [pd.read_csv(i, encoding='latin-1', sep=';') for i in st.session_state.uploaded_files]
+
+    if not files:
+        st.write("Nenhum dado encontrado nos arquivos.")
+        return
+
+    st.session_state.df = pd.concat(files, ignore_index=True)
+    filtered_df = apply_filters(st.session_state.df, translate_label)
+
+    if filtered_df.empty:
+        st.write("Nenhum dado encontrado com os filtros aplicados.")
+    else:
+        st.write(filtered_df)
+
+def run():
+    st.set_page_config(
+        page_title="Campus PI App", 
+        page_icon="📈", 
+        layout="wide", 
+        initial_sidebar_state="expanded", 
+        menu_items={
+            'Get Help': 'https://www.extremelycoolapp.com/help',
+            'Report a bug': "https://www.extremelycoolapp.com/bug",
+            'About': """
+                Este é um Projeto de Pesquisa desenvolvido para visualização, análise e transparência de dados do ifc araquari.
+                \\
+                \\
+                Professor Responsavel: [Fábio Longo de Moura](www.github.com/ldmfabio) 
+                \\
+                Aluno Responsavel: [Mateus Lopes Albano](www.github.com/mateus-lopes)
+                \
+                \
+
+            """
+        }
+    )
+
+    if "is_running" not in st.session_state:
+        st.session_state.is_running = False       
 
     with st.sidebar:
         st.write("# Opções:")
-        filter_options_status = (
-            "Todas",
-            "Em curso",
-            "Concluinte",
-            "Abandono", 
-            "Desistente",
-            "Transferido",
-        )
-        
-        st.session_state.selected_filter = st.selectbox(
+    
+        st.session_state.selected_filter_status = st.selectbox(
             label="Situação da Matrícula:",
-            options=filter_options_status,
+            options=filters.filter_options_status,
         )
        
-        filter_options_year = (
-            "Todos",
-            "2018",
-            "2019",
-            "2020",
-            "2021",
-            "2022",
-            "2023",
-            "2024",
-        )
-        
-        st.session_state.selected_filter = st.selectbox(
+        st.session_state.selected_filter_year = st.selectbox(
             label="Ciclos:",
-            options=filter_options_year,
+            options=filters.filter_options_year,
         )
         
-        filter_options_curse = (
-            "Todos",
-            "Técnico em Informática",
-            "Técnico em Eletromecânica",
-            "Técnico em Mecânica",
-        )
-        
-        st.session_state.selected_filter = st.selectbox(
+        st.session_state.selected_filter_curse = st.selectbox(
             label="Cursos:",
-            options=filter_options_curse,
+            options=filters.filter_options_curse,
         )
 
         st.session_state.uploaded_files = st.file_uploader("Escolha os Arquivos CSV", accept_multiple_files=True, help="Arraste e solte os arquivos aqui ou clique para fazer upload.")
@@ -121,7 +126,7 @@ def run():
         st.write("*Versão 1.0.1*")
 
     if st.session_state.is_running:
-        runGraph()
+        runGraph(filters.translate_label)
     else:
         runPage()
 
